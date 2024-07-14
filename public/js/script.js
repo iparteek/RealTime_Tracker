53563720cc781797b7109f9db87d2d42693d8e05
iparteek <prateek10242@gmail.com>
2024-07-15 00:38:01 +0530

    main

---
+++ public/js/script.js
@@ -0,0 +1,48 @@
+const socket = io();
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            console.log("Position:", position);
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (error) => {
+            console.error("Geolocation error:", error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        }
+    );
+} else {
+    console.error("Geolocation is not supported by this browser.");
+}
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Igris"
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    console.log("Location received from server:", data);
+    map.setView([latitude, longitude], 16);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected", (id) => {
+    console.log("User disconnected:", id);
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
